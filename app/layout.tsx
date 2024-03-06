1619bebf025b04be3157aadae67aa740b8466513
sabbCodes <sarafaabbas@gmail.com>
2024-03-06 08:36:40 +0100

    metadata added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,25 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  authors: [
+    {
+      name: 'Sabb',
+      url: 'https://sabb-portfolio-v2.netlify.app/'
+    },
+    {
+      name: 'Next.js',
+      url: 'https://nextjs.org'
+    },
+  ]
+};
 
 export default function RootLayout({
   children,
